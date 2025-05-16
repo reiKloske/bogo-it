b7a4770d250eff3c89ea28062689e5a8f4abdf25
reiKloske <lanafofa2@gmail.com>
2025-05-16 20:38:42 +0100

    Logs addition

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from main import app, start_bogo_sort
 
-print("WSGI entry point: Initializing application...")
+app.logger.info("WSGI entry point: Initializing application...")
 start_bogo_sort()
+app.logger.info("WSGI entry point: Application initialized successfully.")
